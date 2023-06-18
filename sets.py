e66865f225ac26806c34e1c77a2266bbc465969d
stefoufouandco <mohamedabououthmane@yahoo.fr>
2023-06-18 21:24:33 +0200

    depot initial

---
+++ sets.py
@@ -0,0 +1,9 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+age_set = set(age)
+print(age, len(age))
+print(age_set, len(age_set))
